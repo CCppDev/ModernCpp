9441a944bfdcb5e33e8efc02ff7e33e900f2abdc
rongwei.li <rongwei.li@jutze.com.cn>
2024-01-04 10:30:50 +0800

    test
    (cherry picked from commit 8d778222627fafedb62d5dda18b0799c3416d894)

--- Cpp11/Chapter2_MoreEffective/MoreEffective_6_move_constructor.cpp
+++ Cpp11/Chapter2_MoreEffective/MoreEffective_6_move_constructor.cpp
@@ -8,49 +8,93 @@ using namespace std;
 // 抄近路
 
 class HasPtrMem {
-    public:
-        // 默认构造
-        HasPtrMem() : _d(new int(100)) {
-            cout << "HasPtrMem() " << this << endl;
-        }
+public:
+    // 默认构造
+    HasPtrMem() : _d(new int(100)) {
+        cout << "HasPtrMem() " << this << endl;
+    }
 
-        // 拷贝构造
-        HasPtrMem(const HasPtrMem &another) : _d(new int(*another._d)) {
-            cout << "HasPtrMem(const HasPtrMem &another) " << this << " -> " << &another << endl;
-        }
+    // 拷贝构造
+    HasPtrMem(const HasPtrMem &another) : _d(new int(*another._d)) {
+        cout << "HasPtrMem(const HasPtrMem &another) " << this << " -> " << &another << endl;
+    }
 
-        // 移动构造
-        HasPtrMem(HasPtrMem &&another) {
-            cout << "HasPtrMem(HasPtrMem &&another) " << this << " -> " << &another << endl;
+    // 移动构造
+    HasPtrMem(HasPtrMem &&another) noexcept {
+        cout << "HasPtrMem(HasPtrMem &&another) " << this << " -> " << &another << endl;
+        this->_d = another._d;
+        another._d = nullptr;
+    }
+
+    HasPtrMem &operator=(HasPtrMem &&another) noexcept {
+        cout << "HasPtrMem& operator=(HasPtrMem &&another) " << this << " -> " << &another << endl;
+        if (this != &another) {
+            delete _d;
             this->_d = another._d;
             another._d = nullptr;
         }
+        return *this;
+    }
 
-        ~HasPtrMem() {
-            if (nullptr != _d) {
-                delete _d;
-            }
-            cout << "~HasPtrMem() " << this << endl;
+    ~HasPtrMem() {
+        if (nullptr != _d) {
+            delete _d;
         }
-        int *_d;
+        cout << "~HasPtrMem() " << this << endl;
+    }
+
+    void show() {
+        if (this->_d == nullptr) {
+            cout << "_d is nullptr" << endl;
+        } else {
+            cout << "show: " << this->_d << endl;
+        }
+    }
+
+    int *_d;
 };
 
-HasPtrMem getTemp() {
-    return HasPtrMem();
+HasPtrMem move_hasPtrMem(HasPtrMem &&hasPtrMem) {
+    cout << "move_hasPtrMem" << endl;
+    HasPtrMem h = static_cast<HasPtrMem &&>(hasPtrMem);
+    cout << "move_hasPtrMem: " << &h << endl;
+    return h;
 }
+
+//HasPtrMem getTemp() {
+//    return HasPtrMem();
+//}
+
+HasPtrMem getTemp() {
+    HasPtrMem hasPtrMem;
+    return hasPtrMem;
+}
+
+HasPtrMem getTemp3(HasPtrMem &&hasPtrMem) {
+    return hasPtrMem;
+}
+
 
 int main() {
 
-    // HasPtrMem a;
-    // HasPtrMem b(a);
+//    HasPtrMem a;
+//    cout << "a's addr: " << &a << endl;
+    move_hasPtrMem(getTemp());
+//     HasPtrMem b(a);
+//    HasPtrMem b = a;
+//    HasPtrMem c = static_cast<HasPtrMem &&>(a); // a 失去了自己的数据 a._d == nullptr
+//
+//    cout << "a's " ;
+//    a.show();
+
 
     // cout << a._d << endl;
     // cout << b._d << endl;
 
-    HasPtrMem &&ret = getTemp();
-    cout << "--------" << endl;
-    cout << &ret << endl;
-    cout << "--------" << endl;
+//    HasPtrMem &&ret = getTemp();
+//    cout << "--------" << endl;
+//    cout << &ret << endl;
+//    cout << "--------" << endl;
 
     return 0;
 }
